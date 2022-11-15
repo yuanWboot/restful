acef3feeb3dc3e0f75d9ba14a217d711e84572de
zy <1661996964@qq.com>
2022-11-15 23:23:00 +0800

    RestController注解与路径变量

--- src/main/java/com/zy/restful/controller/RestfulController.java
+++ src/main/java/com/zy/restful/controller/RestfulController.java
@@ -3,29 +3,30 @@ package com.zy.restful.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/restful")
 public class RestfulController {
     @GetMapping("/request")
-    @ResponseBody
+    //@ResponseBody
     public String doGetRequest() {
         return "{\"message\":\"返回查询结果\"}";
     }
 
-    @PostMapping("/request")
-    @ResponseBody
-    public String doPostRequest() {
-        return "{\"message\":\"数据新建成功\"}";
+    //POST /restful/request/100
+    @PostMapping("/request/{rid}")
+   // @ResponseBody
+    public String doPostRequest(@PathVariable("rid") Integer requestId) {
+        return "{\"message\":\"数据新建成功\",\"id\":"+requestId +"}";
     }
 
     @PutMapping("/request")
-    @ResponseBody
+    //@ResponseBody
     public String doPutRequest() {
         return "{\"message\":\"数据更新成功\"}";
     }
 
     @DeleteMapping("/request")
-    @ResponseBody
+    //@ResponseBody
     public String doDeleteRequest() {
         return "{\"message\":\"数据删除成功\"}";
     }
