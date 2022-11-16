13f5c9e8be5947f63de91ebc3df3aa0eb8c9bd32
zy <1661996964@qq.com>
2022-11-16 09:32:56 +0800

    json序列化多条数据 和 json格式化时间

--- src/main/java/com/zy/restful/controller/RestfulController.java
+++ src/main/java/com/zy/restful/controller/RestfulController.java
@@ -4,6 +4,10 @@ import com.zy.restful.entiry.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 @RestController
 @RequestMapping("/restful")
 public class RestfulController {
@@ -15,16 +19,16 @@ public class RestfulController {
 
     //POST /restful/request/100
     @PostMapping("/request/{rid}")
-   // @ResponseBody
+    // @ResponseBody
     public String doPostRequest(@PathVariable("rid") Integer requestId, Person person) {
-        System.out.println(person.getName()+":"+person.getAge());
-        return "{\"message\":\"数据新建成功\",\"id\":"+requestId +"}";
+        System.out.println(person.getName() + ":" + person.getAge());
+        return "{\"message\":\"数据新建成功\",\"id\":" + requestId + "}";
     }
 
     @PutMapping("/request")
     //@ResponseBody
     public String doPutRequest(Person person) {
-        System.out.println(person.getName()+":"+person.getAge());
+        System.out.println(person.getName() + ":" + person.getAge());
         return "{\"message\":\"数据更新成功\"}";
     }
 
@@ -34,16 +38,37 @@ public class RestfulController {
         return "{\"message\":\"数据删除成功\"}";
     }
 
+    //json序列化单条数据
     @GetMapping("/person")  //http://localhost/restful/person?id=1
     public Person findPersonById(Integer id) {
         Person person = new Person();
         if (id == 1) {
             person.setName("张三");
             person.setAge(66);
-        }else if (id == 2) {
+        } else if (id == 2) {
             person.setName("李四");
             person.setAge(18);
         }
         return person;
     }
+
+    //json序列化多条数据
+    @GetMapping("/persons")  // http://localhost/restful/persons
+    public List<Person> findPersons() {
+        List list = new ArrayList();
+        Person p1 = new Person();
+        p1.setName("张三");
+        p1.setAge(66);
+        p1.setBirthday(new Date());
+
+        Person p2 = new Person();
+        p2.setName("李四");
+        p2.setAge(22);
+        p2.setBirthday(new Date());
+
+        list.add(p1);
+        list.add(p2);
+        return list;
+    }
+
 }
--- src/main/java/com/zy/restful/entiry/Person.java
+++ src/main/java/com/zy/restful/entiry/Person.java
@@ -1,8 +1,15 @@
 package com.zy.restful.entiry;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
 public class Person {
     private String name ;
     private Integer age;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date birthday;
 
     public String getName() {
         return name;
@@ -18,5 +25,13 @@ public class Person {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 }
